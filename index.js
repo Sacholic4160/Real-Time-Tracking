01a899a7d2e70d92bc190ae3409b1a0313d92d10
Sachin Parmar <sachinparmar4160@gmail.com>
2024-07-22 18:52:45 +0530

    done some modification for the real time location updation

--- index.js
+++ index.js
@@ -1,18 +1,62 @@
-const express = require('express')
+const express = require('express');
+const http = require('http');
+const socketio = require('socket.io');
+const path = require('path');
+const sequelize = require('./config/db');
+const Rider = require('./models/rider.model.js');
+
 const app = express();
-const http = require('http')
-
-const socketio = require('socket.io')
-
 const server = http.createServer(app);
-
 const io = socketio(server);
 
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
 
+sequelize.sync();
 
-app.get('/', (req,res) => {
-    res.send('hey')
+app.get('/', async (req, res) => {
+    const riders = await Rider.findAll();
+    res.render('index', { riders });
+});
+
+app.post('/add-rider',  async(req,res) => {
+    const {name, latitude, longitude } = req.body;
+    const newRider = await Rider.create({name, latitude, longitude})
+     res.status(201).json(newRider)
 })
+
+app.post('/update-location', async (req,res) => {
+    const {id, latitude, longitude }= req.body;
+    const rider = await Rider.findByPk(id);
+    if (rider) {
+        rider.latitude = latitude;
+        rider.longitude = longitude;
+        rider.last_updated = new Date();
+        await rider.save();
+        io.emit('update-location', { id, latitude, longitude });
+        res.status(200).send('Location updated');
+    } else {
+        res.status(404).send('Rider not found');
+    }
+   
+})
+
+// WebSocket connection
+io.on('connection', (socket) => {
+    console.log('New user connected');
+    
+    // Send all riders' locations on connection
+    Rider.findAll().then(riders => {
+        socket.emit('initial-locations', riders);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    });
+});
+
+
 
 server.listen(3000,() => {
     console.log('server is running')
